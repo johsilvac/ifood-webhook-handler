1d624d089aee6206a210f19375f823b1d1143422
johsilvac <jonathan.cardoso@live.com>
2025-04-10 21:13:47 -0300

    Delete api/ifood-webhook.js

--- api/ifood-webhook.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('Recebido do iFood:', req.body);
-    res.status(200).json({ message: 'Recebido com sucesso!' });
-  } else {
-    res.status(405).json({ error: 'Método não permitido' });
-  }
-}
